89faf80917758a3a49e488cf5f1af6a6dc8ac2b2
Felix Mumo <kifelx@gmail.com>
2022-03-10 21:05:07 +0000

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  * main - Prints out the last part of a quote in the standard error.
  * - Dora Korpar, 2015-10-19
  * followed by a new line, to the standards error
- * Return: Always 1 (Success)
+ * Return: Always 1=0 (Success)
  */
 
 int main(void)
