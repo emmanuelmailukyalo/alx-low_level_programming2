a2e49b37b36c966243bfecc9f447652c274b8da3
Felix Mumo <kifelx@gmail.com>
2022-03-10 21:01:29 +0000

    NANO

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
+
 /**
  * main - Prints out the last part of a quote in the standard error.
- * Return: 1 if success.
+ * - Dora Korpar, 2015-10-19
+ * followed by a new line, to the standards error
+ * Return: Always 1 (Success)
  */
+
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
