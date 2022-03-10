ec52cfdb77c1fbb7215e0db2515968a7f54799e8
Felix Mumo <kifelx@gmail.com>
2022-03-10 10:03:04 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ *Main is printing out what I had written
+ */
+
 int main()
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+  	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
