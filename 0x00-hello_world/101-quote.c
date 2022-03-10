01c2e3d134fab09a0ae258a73dcb2066ea058846
Felix Mumo <kifelx@gmail.com>
2022-03-10 21:16:30 +0000

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdarg.h>
 
 /**
  * main - Prints out the last part of a quote in the standard error.
@@ -11,6 +10,6 @@
 
 int main(void)
 {
-        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
-        return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
