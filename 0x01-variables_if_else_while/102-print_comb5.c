de3510c82b322dab3cc9eb49ff75a3efe9ecf0c6
Felix Mumo <kifelx@gmail.com>
2022-03-12 19:43:20 +0000

    Fixed last qn

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,50 +1,36 @@
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+#include <stdlib.h>
+#include <time.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
- */
+ * Return: Always 0
+*/
+
 int main(void)
 {
-	int p = 48, q = 48, y = 48, x = 49;
+	int p, q;
 
-	while (p < 58)
+	for (p = 0; p <= 98; p++)
 	{
-		while (q < 58)
+		for (q = p + 1; q <= 99; q++)
 		{
-			while (y < 58)
-			{
-				while (x < 58)
-				{
-					putchar(p);
-					putchar(q);
-					putchar(' ');
-					putchar(y);
-					putchar(x);
-					if (!(p == 57 &&
-						q == 56 &&
-						y == 57 &&
-						x == 57))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					x++;
-				}
-				y++;
-				x = 48;
-			}
-			q++;
-			q = p;
-			x = q + 1;
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			putchar(' ');
+			putchar((q / 10) + '0');
+			putchar((q % 10) + '0');
+
+			if (p == 98 && q == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
-		p++;
-		q = 48;
-		y = p;
-		x = q+ 1;
 	}
+
 	putchar('\n');
+
 	return (0);
 }
