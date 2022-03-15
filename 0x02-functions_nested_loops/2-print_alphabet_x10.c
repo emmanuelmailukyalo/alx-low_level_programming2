79c64b6b38e34b3031129e2fda6d18752350ad6a
Felix Mumo <kifelx@gmail.com>
2022-03-15 15:04:14 +0000

    Print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@
  */
 void print_alphabet_x10(void)
 {
-	int count = 0;
+	int count;
 	char letter;
 
-	while (count++ <= 9)
+	for (count = 0; count <= 9; count++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
@@ -20,5 +20,4 @@ void print_alphabet_x10(void)
 		}
 		_putchar('\n');
 	}
-	return (0);
 }
