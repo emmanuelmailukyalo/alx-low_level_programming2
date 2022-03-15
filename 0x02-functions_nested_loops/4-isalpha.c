2cc92a0703ac49f635d0468f1257dffa82c800e6
Felix Mumo <kifelx@gmail.com>
2022-03-15 17:56:33 +0000

    is an alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,11 +4,12 @@
  * _isalpha - Checks if a character is alphabetic.
  * @c: The character to be checked.
  *
- * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
+ * Return: 1 if character is a letter, lowercase or uppercase, 0 otherwise.
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c <= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') ||
+	    (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
