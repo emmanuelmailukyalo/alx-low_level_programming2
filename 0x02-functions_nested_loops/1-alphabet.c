2d35a816a9b12d83939129aab99ebad38fd7dc47
Felix Mumo <kifelx@gmail.com>
2022-03-15 09:32:21 +0000

    Putc

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <main.h>
+
+/**
+ * main - void print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	
+	return 0;
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 #0. Writing a program that prints _putchar followed by a new line
+#1. Writing a function that prints the alphabet in lowercase
