b94ec361d0f48e05495d94b8deab6c807b43aced
Felix Mumo <kifelx@gmail.com>
2022-03-16 06:58:12 +0000

    Largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,14 +12,17 @@ int largest_number(int a, int b, int c)
 
 	if (a > b && b > c)
 	{
+		largest = a;
 		printf("%d", a);
 	}
 	else if (b > a && a > c)
 	{
+		largest = b;
 		printf("%d". b);
 	}
 	else
 	{
+		largest = c;
 		printf("%d", c)
 	}
 
