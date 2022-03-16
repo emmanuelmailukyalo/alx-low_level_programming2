886c6a3c9a3c7fcaea30267e93c994da6206030e
Felix Mumo <kifelx@gmail.com>
2022-03-16 06:56:16 +0000

    Largest

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,27 @@
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a > b && b > c)
+	{
+		printf("%d", a);
+	}
+	else if (b > a && a > c)
+	{
+		printf("%d". b);
+	}
+	else
+	{
+		printf("%d", c)
+	}
+
+	return (largest);
+}
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,2 +1,3 @@
 #0. Finding bugs in posisive or negative function
 #1. Commenting out the part of the code that makes the code go on an infinite loop
+#2. Debugging a program that prints the largest of three integers
