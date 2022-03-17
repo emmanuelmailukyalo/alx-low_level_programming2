99e0967c63f4f888e2ada01ba922209e24aa8c77
Felix Mumo <kifelx@gmail.com>
2022-03-17 10:48:54 +0000

    Print numbers fixed

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - Prints the numbers from 0-9.
+ * print_numbers - print 0-9 and end with new line.
+ *
+ * Return: 0-9and new line after
  */
 void print_numbers(void)
 {
-	int num;
-
-	for (num = 0; num < 10; num++)
-		_putchar((num % 10) + '0');
-
-	_putchar('\n');
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
 }
