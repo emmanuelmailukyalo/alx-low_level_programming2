348356494e3dab4fbf5b167c8956aad1637defe5
Felix Mumo <kifelx@gmail.com>
2022-03-24 10:57:36 +0000

    Mozart

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,81 +1,31 @@
 #include "main.h"
 
-int is_sep(char c);
-char cap_char(char c);
-
 /**
-* cap_string - capitalizes all words of a string
-* @str: string whose wordsto capitalize
+* leet -  encodes a string into 1337
+* @str: string to encode
 *
-* Description:
-* Separators of words: space, tabulation, new line,
-* ,, ;, ., !, ?, ", (, ), {, and }
-*
-* Return: pointer to str whose words have been capitalized
+* Return: pointer to encoded str
 */
-char *cap_string(char *str)
+char *leet(char *str)
 {
+	/* conversion tables */
+	char letters[] = "aeotl";
+	char values[] = "43071";
+
 	int i;
-	int sep_prev = 0;
+	int j;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		/* check if the current charater is a separator */
-		if (is_sep(str[i]))
+		for (j = 0; letters[j] != '\0'; j++)
 		{
-			sep_prev = 1;
-		}
-		/* check if the previous character was a separator */
-		else if (sep_prev)
-		{
-			str[i] = cap_char(str[i]);
-			sep_prev = 0;
+			if (str[i] == letters[j] || str[i] == letters[j] - 32)
+			{
+				str[i] = values[j];
+				break;
+			}
 		}
 	}
-
-	/* capitalize the first character if string is non-empty */
-	if (i > 0)
-		str[0] = cap_char(str[0]);
 
 	return (str);
-}
-
-/**
-* is_sep - checks whether a given character is a separator
-* @c: character to be checked
-*
-* Description:
-* Separators are: space, tabulation, new line,
-* ,, ;, ., !, ?, ", (, ), {, and }
-*
-* Return: 1 if c is a separator. 0 otherwise
-*/
-int is_sep(char c)
-{
-	char seps[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '\"', '(', ')',
-				'{', '}'};
-	int n = sizeof(seps) / sizeof(seps[0]);
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		if (c == seps[i])
-			return (1);
-	}
-
-	return (0);
-}
-
-/**
-* cap_char - gets the uppercase value of a character
-* @c: character whose uppercase value to find
-*
-* Return: the uppercase vallue of c if c is in lowercase. Otherwise c
-*/
-char cap_char(char c)
-{
-	if (c >= 'a' && c <= 'z')
-		return (c - 32);
-
-	return (c);
 }
